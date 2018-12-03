98011c051c2c0761156498c4fc3dbb69013523e9
Ryan <rsaridar@outlook.com>
2018-12-03 18:03:37 +0000

    Added my disable_duplicate_step_sound addon

---
+++ disable_duplicate_step_sound/plugin/sv_hooks.lua
@@ -0,0 +1,15 @@
+--[[function PLUGIN:EntityEmitSound(soundData)
+	if (!soundData.Entity:IsPlayer()) then
+		return;
+	end;
+
+	local soundName = soundData.OriginalSoundName;
+	local blockedSuffixes = { ".stepleft", ".stepright" };
+	
+	for k, v in pairs(blockedSuffixes) do
+		if (soundName:sub(-#v) == v) then
+			return false;
+		end;
+	end;
+end;
+]]
